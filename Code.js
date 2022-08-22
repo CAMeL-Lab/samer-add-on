d266e8f0870b0123eda1283e3ae0c1369975e57e
reem-hazim <reem.hazim02@gmail.com>
2022-08-22 14:00:37 +0400

    Changed markup functions to allow user selection

--- Code.js
+++ Code.js
@@ -208,32 +208,88 @@ function clearAnnotation(){
   }
 }
 
-function minimizeMarkup(mrkpList){
-  const text = DocumentApp.getActiveDocument().getBody().editAsText();
-  // loop through each word
-  for (let i = 0; i < mrkpList.length; i++) {
-    // select word text
-    let word = text.getText().substring(mrkpList[i].idx, mrkpList[i].endidx);
-    // check if the text already has markup
-    let found_index = /#[٠١٢٣٤٥٦]#/g.exec(word)
-    console.log(found_index)
-    if (found_index != null)
-        // minimize font
-        // text.setFontSize(mrkpList[i].idx, mrkpList[i].idx + 2, 1);
-        text.setFontSize(mrkpList[i].idx + found_index.index, mrkpList[i].idx + found_index.index + 2, 1);
+function editTextBasedMarkupList(func, mrkpList, startOffset, add_args){
+  const selection = DocumentApp.getActiveDocument().getSelection();
+  
+  if (selection) {
+    const rangeElements = selection.getRangeElements();
+    
+    let startMrkpIdx = 0;
+    let startIdx = startOffset;
+    let adjustStart = 0;
+    let rawtext;
 
+    //Each paragraph is an element
+    for (let rangeElement of rangeElements) {
+      // check if element is text
+      if (rangeElement.getElement().editAsText) {
+        // get text of element
+        let element = rangeElement.getElement();
+        text = element.asText().editAsText();
+        
+        let res = func(mrkpList,text,startMrkpIdx,startIdx, adjustStart, add_args);
+        startMrkpIdx = res[0]
+        add_args = res[1]
+
+        if(!rawtext){
+          rawtext = text.getText()
+        } else
+          rawtext = rawtext + " " + text.getText();
+        adjustStart = rawtext.length + 1; 
+      }
+    }
+  } else {
+     text = DocumentApp.getActiveDocument().getBody().editAsText(); //$("div.content").text();
+     func(mrkpList,text,0,0, 0, add_args);
   }
 }
 
-function hideMarkup(mrkpList){
-  const text = DocumentApp.getActiveDocument().getBody().editAsText();
-  let adjustWordIdx = 0;
+function minimizeMarkup(mrkpList, startOffset){
+  return editTextBasedMarkupList(minimizeMarkupInternal,mrkpList, startOffset, {})
+}
+
+function minimizeMarkupInternal(mrkpList, text,startMrkpIdx, startIdx, adjustStart, add_args){
+  let i;
+  let textLength = text.getText().length 
+  // loop through each word
+  for (i = startMrkpIdx; i < mrkpList.length && (mrkpList[i].endidx - adjustStart + startIdx) <= textLength; i++){
+    let start = mrkpList[i].idx - adjustStart + startIdx
+    let end = mrkpList[i].endidx - adjustStart + startIdx
+    // select word text
+    let word = text.getText().substring(start, end+1);
+    Logger.log(`word: ${word}, start idx: ${start}, end idx: ${end}`)
+    // check if the text already has markup
+    let found_index = /#[٠١٢٣٤٥٦]#/g.exec(word)
+    if (found_index != null){
+        // minimize font
+      console.log(found_index)
+        text.setFontSize(start + found_index.index, start + found_index.index + 2, 1);
+    }
+  }
+  return [i, null];
+}
+
+function hideMarkup(mrkpList, startOffset){
+  return editTextBasedMarkupList(hideMarkupInternal, mrkpList, startOffset, {"adjustWordIdx":0})
+}
+
+function hideMarkupInternal(mrkpList, text,startMrkpIdx, startIdx, adjustStart, add_args){
+  let i;
+  let textLength = text.getText().length 
+  let {adjustWordIdx} = add_args;
+  let currentFnAdjustWordIdx = 0
+  Logger.log(`new call to showMarkupInternal with textlength: ${textLength}`)
 
   // loop through each word
-  for (let i = 0; i < mrkpList.length; i++) {
+  for (i = startMrkpIdx; i < mrkpList.length && (mrkpList[i].endidx - adjustStart + startIdx) <= textLength + currentFnAdjustWordIdx; i++){
+    
+    let start = mrkpList[i].idx - adjustStart + startIdx
+    let end = mrkpList[i].endidx - adjustStart + startIdx
     let editText = text.getText()
     // select word text
-    let word = editText.substring(mrkpList[i].idx, mrkpList[i].endidx);
+    let word = editText.substring(start, end+1);
+    Logger.log(`i: ${i}, word: ${word}, start: ${start}, end: ${end}, word.idx: ${mrkpList[i].idx}, word.endidx: ${mrkpList[i].endidx}`)
+    
     // check if the text already has markup
     let hash_match = /#[٠١٢٣٤٥٦]#/g.exec(word)
     if (hash_match != null){
@@ -241,40 +297,52 @@ function hideMarkup(mrkpList){
       if(mrkpList[i].lvl === mrkpList[i].actual_lvl){
         console.log(mrkpList[i].str, mrkpList[i].lvl, mrkpList[i].actual_lvl)
         // delete markup
-        text.deleteText(hash_match.index + mrkpList[i].idx, hash_match.index + mrkpList[i].idx + 2);
+        text.deleteText(hash_match.index + start, hash_match.index + start + 2);
         // update future word indices
         adjustWordIdx += 3;
+        currentFnAdjustWordIdx -= 3
         mrkpList[i].endidx -= 3;
         // if the word is inconsistent with auto markup
       } else { 
         // delete old hash
-        text.deleteText(hash_match.index + mrkpList[i].idx, hash_match.index + mrkpList[i].idx + 2)
+        text.deleteText(hash_match.index + start, hash_match.index + start + 2)
         // insert new hash
-        text.insertText(hash_match.index + mrkpList[i].idx, `#${eng_to_ar[mrkpList[i].lvl]}#`)
+        text.insertText(hash_match.index + start, `#${eng_to_ar[mrkpList[i].lvl]}#`)
         // minimize font
-        text.setFontSize(hash_match.index + mrkpList[i].idx, hash_match.index + mrkpList[i].idx + 2, 1);
+        text.setFontSize(hash_match.index + start, hash_match.index + start + 2, 1);
       }
     }
     // update word indices
     if (i+1 < mrkpList.length){
       mrkpList[i+1].idx -= adjustWordIdx;
       mrkpList[i+1].endidx -= adjustWordIdx;
-
     }
-    
   }
+  return [i, {adjustWordIdx}]
 }
 
-function clearMarkup(mrkpList){
-  let adjustWordIdx = 0;
-  const text = DocumentApp.getActiveDocument().getBody().editAsText();
+function clearMarkup(mrkpList, startOffset){
+  return editTextBasedMarkupList(clearMarkupInternal, mrkpList, startOffset, {"adjustWordIdx":0})
+}
 
-    for (let i = 0; i < mrkpList.length; i++) {
-      const word = text.getText().substring(mrkpList[i].idx, mrkpList[i].endidx);
+// todo: check if hash index is within the word
+function clearMarkupInternal(mrkpList,text,startMrkpIdx,startIdx, adjustStart, add_args){
+  let textLength = text.getText().length 
+  let {adjustWordIdx} = add_args;
+  let i;
+  let currentFnAdjustWordIdx = 0
+
+  for (i = startMrkpIdx; i < mrkpList.length && (mrkpList[i].endidx - adjustStart + startIdx) <= textLength + currentFnAdjustWordIdx; i++){
+    let start = mrkpList[i].idx - adjustStart + startIdx
+    let end = mrkpList[i].endidx - adjustStart + startIdx
+
+    const word = text.getText().substring(start,end + 1);
       // check if the text already has markup
-      if (word.match(/#[٠١٢٣٤٥٦]#/g)){
-        text.deleteText(mrkpList[i].idx, mrkpList[i].idx + 2);
+    let hash_match = /#[٠١٢٣٤٥٦]#/g.exec(word)
+    if (hash_match != null){
+        text.deleteText(start + hash_match.index, start + hash_match.index + 2);
         adjustWordIdx += 3;
+        currentFnAdjustWordIdx -= 3;
         mrkpList[i].endidx -= 3;
       } 
       if (i+1 < mrkpList.length){
@@ -282,34 +350,47 @@ function clearMarkup(mrkpList){
         mrkpList[i+1].endidx -= adjustWordIdx;
       }
     }
+  return [i, {adjustWordIdx}]
 }
 
+function showMarkup(mrkpList, startOffset){
+  return editTextBasedMarkupList(showMarkupInternal, mrkpList, startOffset, {"adjustWordIdx":0})
+}
 
-function showMarkup(mrkpList){
-  let adjustWordIdx = 0;
-  const text = DocumentApp.getActiveDocument().getBody().editAsText();
+function showMarkupInternal(mrkpList,text,startMrkpIdx,startIdx, adjustStart, add_args){
+  let textLength = text.getText().length 
+  let {adjustWordIdx} = add_args;
+  let i;
+  let currentFnAdjustWordIdx = 0
+  Logger.log(`new call to showMarkupInternal with textlength: ${textLength}`)
+  for (i = startMrkpIdx; i < mrkpList.length && (mrkpList[i].endidx - adjustStart + startIdx) <= textLength + currentFnAdjustWordIdx; i++){
+    let start = mrkpList[i].idx - adjustStart + startIdx
+    let end = mrkpList[i].endidx - adjustStart + startIdx
+    const word = text.getText().substring(start, end + 1);
+    Logger.log(`i: ${i}, word: ${word}, start: ${start}, end: ${end}, word.idx: ${mrkpList[i].idx}, word.endidx: ${mrkpList[i].endidx}`)
 
-    for (let i = 0; i < mrkpList.length; i++) {
-      const word = text.getText().substring(mrkpList[i].idx, mrkpList[i].endidx);
-      // check if the text already has markup
-      if (!word.match(/#[٠١٢٣٤٥٦]#/g)){
-        // insert markup
-        text.insertText(mrkpList[i].idx, `#${eng_to_ar[mrkpList[i].lvl]}#`);
-        // adjust the indices of next words
-        adjustWordIdx += 3;
-        mrkpList[i].endidx += 3;
+    // check if the text already has markup
+    let hash_match = /#[٠١٢٣٤٥٦]#/g.exec(word)
+    if (hash_match == null){
+      // insert markup
+      text.insertText(start, `#${eng_to_ar[mrkpList[i].lvl]}#`);
+      // adjust the indices of next words
+      adjustWordIdx += 3;
+      currentFnAdjustWordIdx += 3
+      mrkpList[i].endidx += 3;
 
-        // if text already has markup, update the font size
-      } else {
-        text.setFontSize(mrkpList[i].idx, mrkpList[i].idx + 2, text.getFontSize(mrkpList[i].idx + 3))
-      }
-
-      // adjust index of next word
-      if (i+1 < mrkpList.length){
-        mrkpList[i+1].idx += adjustWordIdx;
-        mrkpList[i+1].endidx += adjustWordIdx;
-      }
+      // if text already has markup, update the font size
+    } else {
+      text.setFontSize(start + hash_match.index, start + hash_match.index + 2, text.getFontSize(start + hash_match.index + 3))
     }
+
+    // adjust index of next word
+    if (i+1 < mrkpList.length){
+      mrkpList[i+1].idx += adjustWordIdx;
+      mrkpList[i+1].endidx += adjustWordIdx;
+    }
+  }
+  return [i, {adjustWordIdx}]
 }
 
 // annotate the document
@@ -335,22 +416,16 @@ function annotateDoc(mrkpList, startOffset, color, setLevel1){
         // // check if part of paragraph is selected
         if (rangeElement.isPartial()) 
          Logger.log(`actual start offset: ${rangeElement.getStartOffset()}`);
-        // else startIdx = 0;
-        Logger.log(`text ${text}`)
-        Logger.log(`startMrkpIdx: ${startMrkpIdx}`)
-        Logger.log(`startIdx: ${startIdx}`)
 
         startMrkpIdx=annotateText(mrkpList,text,color,startMrkpIdx,startIdx, adjustStart, setLevel1);
         if(!rawtext){
           rawtext = text.getText()
         } else
           rawtext = rawtext + " " + text.getText();
-        Logger.log(`rawtext: ${rawtext}`)
-        adjustStart= rawtext.length + 1; 
+        adjustStart = rawtext.length + 1; 
       }
     }
   } else {
-    Logger.log("here")
      text = DocumentApp.getActiveDocument().getBody().editAsText(); //$("div.content").text();
      annotateText(mrkpList,text,color,0,0, 0, setLevel1);
   }
